bfd689fe53606f35c6d4243547bf7beaa092e653
Nelson Koby Tusco Frias <n42749701@gmail.com>
2026-09-17 15:36:32 -0400

    mi primer commit

---
+++ 6clases13082026/js/miScript.js
@@ -0,0 +1,30 @@
+alert(si funciona);
+let nota3 parseInt(prompt)("int la tercera nota;")
+function multiplicar (a,b){
+     let resultado=0;
+     let contador=0;
+     while(contador<b){
+        resultado=0;
+        contador++;
+     }
+      return resultado;
+}
+let a=Numbr(prompt("intr un numero:"));
+let b=Numbr(prompt("intr un numero"));
+console.log("resultado:"+multiplicar(a,b));
+
+funcion dividion (a,b)
+{
+    let r=0;
+    while(a<b)
+    {
+        a=a-b;
+        r=r+1;
+    }
+    return r;
+}
+    
+
+
+
+
